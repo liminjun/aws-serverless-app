00d0e61e3f60b2a695d976ff02999b8c58b14ff3
liminjun <v-leeli@hotmail.com>
2018-02-01 10:21:33 +0800

    feat:项目初始化

---
+++ src/app/compare/compare.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+
+import { CompareData } from './compare-data.model';
+import { AuthService } from '../user/auth.service';
+
+@Injectable()
+export class CompareService {
+  dataEdited = new BehaviorSubject<boolean>(false);
+  dataIsLoading = new BehaviorSubject<boolean>(false);
+  dataLoaded = new Subject<CompareData[]>();
+  dataLoadFailed = new Subject<boolean>();
+  userData: CompareData;
+  constructor(private http: Http,
+              private authService: AuthService) {
+  }
+
+  onStoreData(data: CompareData) {
+    this.dataLoadFailed.next(false);
+    this.dataIsLoading.next(true);
+    this.dataEdited.next(false);
+    this.userData = data;
+    this.authService.getAuthenticatedUser().getSession((err, session) => {
+      if (err) {
+        return;
+      }
+      this.http.post('https://xar9oly67l.execute-api.ap-northeast-1.amazonaws.com/dev/compare-yourself', data, {
+        headers: new Headers({'Authorization': session.getIdToken().getJwtToken()})
+      })
+        .subscribe(
+          (result) => {
+            this.dataLoadFailed.next(false);
+            this.dataIsLoading.next(false);
+            this.dataEdited.next(true);
+          },
+          (error) => {
+            this.dataIsLoading.next(false);
+            this.dataLoadFailed.next(true);
+            this.dataEdited.next(false);
+          }
+        );
+    });
+  }
+  onRetrieveData(all = true) {
+    this.dataLoaded.next(null);
+    this.dataLoadFailed.next(false);
+    this.authService.getAuthenticatedUser().getSession((err, session) => {
+      const queryParam = '?accessToken=' + session.getAccessToken().getJwtToken();
+      let urlParam = 'all';
+      if (!all) {
+        urlParam = 'single';
+      }
+      this.http.get('https://API_ID.execute-api.REGION.amazonaws.com/dev/compare-yourself/' + urlParam + queryParam, {
+        headers: new Headers({'Authorization': session.getIdToken().getJwtToken()})
+      })
+        .map(
+          (response: Response) => response.json()
+        )
+        .subscribe(
+          (data) => {
+            if (all) {
+              this.dataLoaded.next(data);
+            } else {
+              console.log(data);
+              if (!data) {
+                this.dataLoadFailed.next(true);
+                return;
+              }
+              this.userData = data[0];
+              this.dataEdited.next(true);
+            }
+          },
+          (error) => {
+            console.log(error);
+            this.dataLoadFailed.next(true);
+            this.dataLoaded.next(null);
+          }
+        );
+    });
+  }
+  onDeleteData() {
+    this.dataLoadFailed.next(false);
+    this.authService.getAuthenticatedUser().getSession((err, session) => {
+      this.http.delete('https://API_ID.execute-api.REGION.amazonaws.com/dev/compare-yourself/?accessToken=XXX', {
+        headers: new Headers({'Authorization': session.getIdToken().getJwtToken()})
+      })
+        .subscribe(
+          (data) => {
+            console.log(data);
+          },
+          (error) => this.dataLoadFailed.next(true)
+        );
+    });
+  }
+}
